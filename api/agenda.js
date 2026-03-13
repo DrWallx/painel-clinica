8ffcb96cf88abdad0022096731e0950ace920649
DrWallx <hauxlifec@gmail.com>
2026-03-13 19:20:16 -0300

    Update agenda.js

--- api/agenda.js
+++ api/agenda.js
@@ -2,14 +2,24 @@ export default async function handler(req, res) {
 
 const token = process.env.FEEGOW_TOKEN;
 
-const response = await fetch("https://api.feegow.com/v1/api/appointments",{
+try {
+
+const response = await fetch("https://api.feegow.com/v1/api/appoints",{
+method:"GET",
 headers:{
-"x-access-token": token
+"x-access-token": token,
+"Content-Type":"application/json"
 }
 });
 
 const data = await response.json();
 
 res.status(200).json(data);
+
+} catch(error){
+
+res.status(500).json({error:"Erro ao conectar ao Feegow"});
+
+}
 
 }
