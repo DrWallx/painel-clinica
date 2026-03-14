6f005aebf5cdc622375cd1eb1d1968c59c24698f
DrWallx <hauxlifec@gmail.com>
2026-03-14 08:28:56 -0300

    Update agenda.js

--- api/agenda.js
+++ api/agenda.js
@@ -2,34 +2,58 @@ export default async function handler(req, res) {
 
 const token = process.env.FEEGOW_TOKEN;
 
-try {
+const hoje = new Date().toLocaleDateString("pt-BR").split('/').reverse().join('-');
 
-const hoje = new Date();
-
-const dia = String(hoje.getDate()).padStart(2,'0');
-const mes = String(hoje.getMonth()+1).padStart(2,'0');
-const ano = hoje.getFullYear();
-
-const data = `${dia}-${mes}-${ano}`;
-
-const url = `https://api.feegow.com/v1/api/appoints/search?data_start=${data}&data_end=${data}`;
-
-const response = await fetch(url,{
-method:"GET",
+const agendaResponse = await fetch(
+`https://api.feegow.com/v1/api/appoints/search?data_start=${hoje}&data_end=${hoje}`,
+{
 headers:{
-"x-access-token": token,
-"Content-Type":"application/json"
+"Content-Type":"application/json",
+"x-access-token":token
 }
-});
+}
+);
 
-const result = await response.json();
+const agendaData = await agendaResponse.json();
 
-res.status(200).json(result);
+if(!agendaData.content){
+return res.status(200).json(agendaData);
+}
 
-} catch(error){
+for(const agendamento of agendaData.content){
 
-res.status(500).json({error:"Erro ao conectar ao Feegow"});
+try{
+
+const pacienteResponse = await fetch(
+`https://api.feegow.com/v1/api/patient/search?paciente_id=${agendamento.paciente_id}`,
+{
+headers:{
+"Content-Type":"application/json",
+"x-access-token":token
+}
+}
+);
+
+const pacienteData = await pacienteResponse.json();
+
+if(pacienteData.success && pacienteData.content){
+
+agendamento.paciente_nome = pacienteData.content.nome;
+
+}else{
+
+agendamento.paciente_nome = "Paciente";
 
 }
+
+}catch{
+
+agendamento.paciente_nome = "Paciente";
+
+}
+
+}
+
+res.status(200).json(agendaData);
 
 }
