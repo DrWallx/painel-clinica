19d93547c3ceb39f497ff4097d9325fc12fd47f7
DrWallx <hauxlifec@gmail.com>
2026-03-20 17:02:58 -0300

    Update salvarPaciente.js

--- api/salvarPaciente.js
+++ api/salvarPaciente.js
@@ -8,13 +8,14 @@ export default async function handler(req, res) {
 
   try {
 
-    const {
-      paciente_id,
-      retorno_valido,
-      receitas,
-      exames,
-      notas
-    } = req.body
+   const {
+  paciente_id,
+  retorno_valido,
+  data_limite_retorno,
+  receitas,
+  exames,
+  notas
+} = req.body
 
     if (!paciente_id) {
       return res.status(400).json({ error: "paciente_id obrigatório" })
@@ -25,13 +26,17 @@ export default async function handler(req, res) {
     // 🔥 pega dados atuais
     let data = await kv.get(key) || {}
 
-    /* ===================== */
-    /* RETORNO */
-    /* ===================== */
+  /* ===================== */
+ /* RETORNO */
+ /* ===================== */
 
-    if (retorno_valido !== undefined) {
-      data.retorno_valido = retorno_valido
-    }
+if (retorno_valido !== undefined) {
+  data.retorno_valido = retorno_valido
+}
+
+if (data_limite_retorno !== undefined) {
+  data.data_limite_retorno = data_limite_retorno
+}
 
     /* ===================== */
     /* LISTAS (SEM PERDER DADOS) */
