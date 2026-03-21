c973cef23d8ca114148fd38d39c278f8b5d8718b
DrWallx <hauxlifec@gmail.com>
2026-03-21 18:06:03 -0300

    Update enviarEmail.js

--- api/enviarEmail.js
+++ api/enviarEmail.js
@@ -168,7 +168,7 @@ export default async function handler(req, res) {
 
     await resend.emails.send({
       from: 'Clínica Haux <hauxlife@hauxlife.com.br>',
-      to: emailPaciente,
+      to: [emailPaciente],
       subject: 'Seus documentos',
       html,
       attachments
