d964871f7598ee9a5063d867b5fb3c9ca30eab23
DrWallx <hauxlifec@gmail.com>
2026-04-20 10:42:04 -0300

    .

--- api/testeInsert.js
+++ api/testeInsert.js
@@ -2,37 +2,30 @@ import { createClient } from '@supabase/supabase-js'
 
 export default async function handler(req, res) {
 
-  try {
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  )
 
-    const supabase = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY
-    )
+  const { data, error } = await supabase
+    .from("financeiro_consultas")
+    .insert([
+      {
+        paciente_id: 1,
+        prontuario: "9999",
+        nome: "Valdésio Xavier",
+        valor: 300,
+        data_consulta: "2025-02-15",
+        mes: 2,
+        ano: 2025,
+        pago: true,
+        data_pagamento: "2025-02-15"
+      }
+    ])
 
-    const { data, error } = await supabase
-      .from("financeiro_consultas")
-      .insert([
-        {
-          paciente: "Valdésio Xavier",
-          prontuario: 9999,
-          valor: 300,
-          data: "2025-02-15",
-          status: "pago",
-          data_pagamento: "2025-02-15"
-        }
-      ])
-
-    if (error) {
-      console.error(error)
-      return res.status(500).json(error)
-    }
-
-    return res.json({
-      sucesso: true,
-      data
-    })
-
-  } catch (err) {
-    return res.status(500).json(err.message)
+  if (error) {
+    return res.status(500).json(error)
   }
+
+  return res.json(data)
 }
